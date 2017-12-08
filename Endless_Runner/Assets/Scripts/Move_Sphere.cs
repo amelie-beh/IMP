9299142c056fbe8dbc285ec905c4e928b2eca2f5
Chuchu_chan-PC\Chuchu_chan <isabell-jennifer94@web.de>
2017-12-08 15:53:31 +0100

    Untergrund und selbst bewegende Sphere und Kamera hinzugefügt

---
+++ Endless_Runner/Assets/Scripts/Move_Sphere.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Sphere : MonoBehaviour {
+	
+	public KeyCode moveL;
+	public KeyCode moveR;
+	
+	public float horizVel = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody>().velocity = new Vector3 (horizVel, 0, 4);
+		
+		if (Input.GetKeyDown(moveL)){
+			horizVel = -2;
+			StartCoroutine (stopSlide());
+		}
+		if (Input.GetKeyDown(moveR)){
+			horizVel = 2;
+			StartCoroutine (stopSlide());
+		}
+	}
+	IEnumerator stopSlide(){
+		yield return new WaitForSeconds ( .5f);
+		horizVel = 0;
+	}
+}
