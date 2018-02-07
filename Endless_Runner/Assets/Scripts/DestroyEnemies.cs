f3fe58349c02dc5ccb5d9ede09b5a16d1ab37101
amelie-beh <amelie-behrendt@web.de>
2018-02-07 23:11:49 +0100

    CollisionChecker und DestroyEnemies hinzugefügt

---
+++ Endless_Runner/Assets/Scripts/DestroyEnemies.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemies : MonoBehaviour {
+
+	private int count = 0;
+
+	void OnMouseDown() {
+		count++;
+		if (count == 5) {
+			Destroy (gameObject);
+		}
+	}
+}
