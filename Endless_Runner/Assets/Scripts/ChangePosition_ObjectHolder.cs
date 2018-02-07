c8151e4f10b1c83856c65894bac8a5636ff5f815
amelie-beh <amelie-behrendt@web.de>
2018-02-08 00:10:25 +0100

    ChangePosition_ObjectHolder und Move_ObjectHolder hinzugefügt

---
+++ Endless_Runner/Assets/Scripts/ChangePosition_ObjectHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition_ObjectHolder : MonoBehaviour {
+
+	public GameObject objectHolder;
+
+	// If ObjectHolder is behind the player's sight, the position will be changed to create a loop of ObjectHolders
+	void Update () {
+
+		if (transform.position.z < 0) {
+			objectHolder.transform.position = new Vector3(0.5f, 3, 36);
+		}
+	}
+}
---
+++ Endless_Runner/Assets/Scripts/Move_ObjectHolder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_ObjectHolder : MonoBehaviour {
+
+	//ObjectHolder bewegt sich in Richtung des Players
+	public float movementSpeed;
+
+	void Update(){
+
+		transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+	}
+}
