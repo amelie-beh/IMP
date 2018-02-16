ff274bb8a6aaec7c31dc6950ab466204b88063b2
amelie-beh <amelie-behrendt@web.de>
2018-02-16 19:00:13 +0100

    ObjectHolder_Controller hinzugefügt

---
+++ Endless_Runner/Assets/Scripts/ChangePosition_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition_Controller : MonoBehaviour {
+
+	public GameObject[] ObjectHolders;
+		
+	public void SetRandomActive () {
+		int r = Random.Range (0, 5);
+		if (ObjectHolders [r].activeInHierarchy == false) {
+			ObjectHolders [r].SetActive (true);
+			Debug.Log (r);
+		} else {
+			SetRandomActive ();
+		}
+	}
+}
--- Endless_Runner/Assets/Scripts/ChangePosition_ObjectHolder.cs
+++ Endless_Runner/Assets/Scripts/ChangePosition_ObjectHolder.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class ChangePosition_ObjectHolder : MonoBehaviour {
 
-	public GameObject objectHolder;
+	public GameObject[] objectHolders;
+	public GameObject ObjectHolderController;
+	public ChangePosition_Controller changePostitionController;
 
 	// If ObjectHolder is behind the player's sight, the position will be changed to create a loop of ObjectHolders
 	void Update () {
-
-		if (transform.position.z < 0) {
-			objectHolder.transform.position = new Vector3(0.5f, 3, 54);
+		for (int i = 0; i <= 4; i++) {
+			int j = i % 4;
+			if (objectHolders[j].transform.position.z <= 0) {
+				objectHolders[j].transform.position = new Vector3 (0.5f, 3, 54);
+				objectHolders[j].SetActive (false);
+				changePostitionController = ObjectHolderController.GetComponent<ChangePosition_Controller> ();
+				changePostitionController.SetRandomActive ();
+			}
 		}
 	}
 }
