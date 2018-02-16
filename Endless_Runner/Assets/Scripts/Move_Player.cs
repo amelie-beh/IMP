bfe4e7b877d232c2976c54730277e6e840b0856b
Chuchu_chan-PC\Chuchu_chan <isabell-jennifer94@web.de>
2018-02-16 17:41:09 +0100

    player bewegung verbessert
    Die Jumps besser beeinflussbar gemacht und das wechseln der Lane verschönert

---
+++ Endless_Runner/Assets/Scripts/Move_Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move_Player : MonoBehaviour {
+	
+	private Rigidbody player;
+	
+	public float jumpHeight = -3f;
+	private bool falling = false;
+	
+	public float z = -4;
+	public int laneNum = 0;
+	public string controllocked = "n";
+	public Swipe swipeControls;
+	
+	public float horizVel = 0;
+	
+	void Start () {
+		player = GetComponent<Rigidbody>();
+		
+	}
+	
+	void Update () {
+		player.velocity = new Vector3(horizVel, jumpHeight, z);
+		
+		if ((swipeControls.SwipeLeft)&&(laneNum >= 0)&&(controllocked== "n")){
+			horizVel = -3.3f;
+			controllocked = "y";
+			laneNum--;
+			StartCoroutine(stopSlide());
+		}
+		if ((swipeControls.SwipeRight)&&(laneNum <= 0)&&(controllocked== "n")){
+			horizVel = 3.3f;
+			controllocked = "y";
+			laneNum++;
+			StartCoroutine(stopSlide());
+			//transform.position = new Vector3(laneNum, jump, z);
+		}
+		if ((swipeControls.SwipeUp)&&(controllocked== "n")){
+			controllocked = "y";
+			jumpHeight = 3f;
+            StartCoroutine(stopJump());
+		}
+		
+		//Vector3 newPostition = transform.position;
+		//newPostition.x = laneNum;
+		//transform.position = newPostition;
+		
+		
+	}
+	IEnumerator stopJump(){
+        yield return new WaitForSeconds(0.5F);
+        jumpHeight = -2;
+        controllocked = "n";
+		
+	}
+	IEnumerator stopSlide(){
+        yield return new WaitForSeconds(0.3F);
+        horizVel = 0;
+        controllocked = "n";
+		transform.position = new Vector3(laneNum, 4.37f, z);
+	}
+}
+
+
+		//X  Left and right
+		//Y Up and down
+		//Z Forward and backwards
