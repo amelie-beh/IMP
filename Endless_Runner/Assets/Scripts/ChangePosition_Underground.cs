afea2c21e6e44fa15ca4bfa8610e2e63a38f8e67
amelie-beh <amelie-behrendt@web.de>
2018-02-19 16:41:51 +0100

    ObjectHolders angepasst, neues Script für ChangePosition

---
+++ Endless_Runner/Assets/Scripts/ChangePosition_Underground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition_Underground : MonoBehaviour {
+
+	public GameObject[] objectHolders;
+
+	// If ObjectHolder is behind the player's sight, the position will be changed to create a loop of ObjectHolders
+	void FixedUpdate () {
+		for (int i = 0; i < 3; i++) {
+			if (objectHolders[i].transform.position.z <= -32) {
+				if (i == 0) {
+					float j = objectHolders [2].transform.position.z + 50;
+					objectHolders [i].transform.position = new Vector3 (0.5f, 3, j);
+				} 
+				if (i == 1) {
+					float j = objectHolders [0].transform.position.z + 50;
+					objectHolders [i].transform.position = new Vector3 (0.5f, 3, j);
+				}
+				if (i == 2) {
+					float j = objectHolders [1].transform.position.z + 50;
+					objectHolders [i].transform.position = new Vector3 (0.5f, 3, j);
+				}
+			}
+		}
+	}
+}
