19403ed86be9331468f69d252c27644bc22e0a87
amelie-beh <amelie-behrendt@web.de>
2018-02-19 17:48:15 +0100

    Candy Cane als Hindernis hinzugefügt

---
+++ Endless_Runner/Assets/Scripts/Move_Object_xx.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Object_xx : MonoBehaviour {
+
+	//ObjectHolder bewegt sich in Richtung des Players
+	public float movementSpeed;
+
+	void Update(){
+
+		transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+	}
+}
