d37d0de23151f5935d6a0b0fefaabbf0d840cf0c
Chuchu_chan-PC\Chuchu_chan <isabell-jennifer94@web.de>
2018-02-20 18:18:09 +0100

    move Skript verbessert
    und am lightning was ausprobiert

--- Endless_Runner/Assets/Scripts/Move_Player.cs
+++ Endless_Runner/Assets/Scripts/Move_Player.cs
@@ -10,7 +10,7 @@ public class Move_Player : MonoBehaviour {
 	public float jumpHeight = -3f;
 	
 	public float z = -4;
-	public int laneNum = 0;
+	public float laneNum = 0;
 	public string controllocked = "n";
 	public Swipe swipeControls;
 	public bool isSliding; 
@@ -27,13 +27,13 @@ public class Move_Player : MonoBehaviour {
 		player.velocity = new Vector3(horizVel, jumpHeight, z);
 		
 		if ((swipeControls.SwipeLeft)&&(laneNum >= 0)&&(controllocked== "n")){
+			laneNum--;
 			horizVel = -3.3f;
 			controllocked = "y";
-			laneNum--;
 			StartCoroutine(stopSlide());
 		}
 		if ((swipeControls.SwipeRight)&&(laneNum <= 0)&&(controllocked== "n")){
-			horizVel = 3.3f;
+			horizVel = 3.4f;
 			controllocked = "y";
 			laneNum++;
 			StartCoroutine(stopSlide());	
@@ -41,7 +41,7 @@ public class Move_Player : MonoBehaviour {
 		if ((swipeControls.SwipeUp)&&(controllocked== "n")){
 			controllocked = "y";
 			jumpHeight = 3f;
-            StartCoroutine(stopJump());
+            StartCoroutine(startJump());
 			isJumping = true;
 		}
 		if ((swipeControls.SwipeDown)&&(controllocked== "n")){
@@ -51,18 +51,24 @@ public class Move_Player : MonoBehaviour {
 			StartCoroutine(stopSliding());
 		}
 	}
+	IEnumerator startJump(){
+        yield return new WaitForSeconds(0.5F);
+        jumpHeight = -3.5f;
+		StartCoroutine(stopJump());
+		
+	}
 	IEnumerator stopJump(){
         yield return new WaitForSeconds(0.5F);
-        jumpHeight = -2;
         controllocked = "n";
 		isJumping = false;
-		
+		transform.position = new Vector3(laneNum, 4.37f, z);	
 	}
 	IEnumerator stopSlide(){
         yield return new WaitForSeconds(0.3F);
         horizVel = 0;
         controllocked = "n";
 		transform.position = new Vector3(laneNum, 4.37f, z);
+		Debug.Log(laneNum);
 	}
 	IEnumerator stopSliding(){
         yield return new WaitForSeconds(0.5F);
