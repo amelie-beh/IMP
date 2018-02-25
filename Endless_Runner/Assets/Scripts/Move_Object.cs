12aafefaff050b0decac3cecaaf2ad6ec099376a
amelie-beh <amelie-behrendt@web.de>
2018-02-25 18:01:14 +0100

    Geschwindigkeitsanstieg hinzugefügt

--- Endless_Runner/Assets/Scripts/Move_Object.cs
+++ Endless_Runner/Assets/Scripts/Move_Object.cs
@@ -6,10 +6,35 @@ public class Move_Object : MonoBehaviour {
 
 	//Object is moving in the direction of the player
 
-	public float movementSpeed;
+	private float movementSpeed = 4;
 
-	void Update(){
-
+	void Update() {
+		
 		transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+
+		if (Time.time >= 30) {
+			movementSpeed = 5;
+		}
+
+		if (Time.time >= 60) {
+			movementSpeed = 6;
+		}
+
+		if (Time.time >= 90) {
+			movementSpeed = 7;
+		}
+
+		if (Time.time >= 120) {
+			movementSpeed = 8;
+		}
+
+		if (Time.time >= 150) {
+			movementSpeed = 9;
+		}
+
+		if (Time.time >= 180) {
+			movementSpeed = 10;
+		}
+
 	}
 }
