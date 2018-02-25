a481339598923b832947068954cc6a0b0bcc7c3b
amelie-beh <amelie-behrendt@web.de>
2018-02-25 17:42:52 +0100

    DestroyEnemies Skript verändert, Tap Hindernis verändert

---
+++ Endless_Runner/Assets/Scripts/HideMeshRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMeshRenderer : MonoBehaviour {
+
+	public HideTapObstacle hideTapObstacle;
+		
+	// Update is called once per frame
+	void Update () {
+		if (hideTapObstacle.isActive == false) {
+			MeshRenderer m = gameObject.GetComponent<MeshRenderer> ();
+			m.enabled = false;
+		} else {
+			MeshRenderer m = gameObject.GetComponent<MeshRenderer> ();
+			m.enabled = true;
+			
+		}
+		
+	}
+}
---
+++ Endless_Runner/Assets/Scripts/HideTapObstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTapObstacle : MonoBehaviour {
+
+	//If the object is clicked five times, it will be set as unactive
+
+	private int count = 0;
+	public bool isActive = true;
+
+	void OnMouseDown() {
+		count++;
+		if (count == 5) {
+			isActive = false;
+			Collider c = gameObject.GetComponent<BoxCollider> ();
+			c.enabled = false;
+			StartCoroutine (SetActiveAgain ());
+		}
+	}
+
+	IEnumerator SetActiveAgain() {
+		yield return new WaitForSeconds(5);
+		isActive = true;
+		Collider c = gameObject.GetComponent<BoxCollider> ();
+		c.enabled = true;
+	}
+}
