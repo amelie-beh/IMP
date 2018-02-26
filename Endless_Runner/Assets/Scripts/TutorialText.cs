f59ec5fd8859de3a13d24dce1caa3090545ce020
amelie-beh <amelie-behrendt@web.de>
2018-02-26 16:13:19 +0100

    Tutorial Level hinzugefügt, sowie erster Trigger

---
+++ Endless_Runner/Assets/Scripts/TutorialText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour {
+
+	//If the player enters the trigger, the UI Text will be shown
+
+	public GameObject text;
+
+	void Start () {
+		text.SetActive (false);
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			text.SetActive (true);
+			StartCoroutine (SetInactive ());
+		}
+	}
+
+	IEnumerator SetInactive() {
+		yield return new WaitForSeconds(5);
+		text.SetActive (false);
+	}
+}
