3adf7f6ac9de2150eac4613b7ab172859caffc99
amelie-beh <amelie-behrendt@web.de>
2018-02-26 16:49:12 +0100

    Tutorial weitergebastelt

---
+++ Endless_Runner/Assets/Scripts/Move_Object_Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Object_Tutorial : MonoBehaviour {
+
+	//Object is moving in the direction of the player
+
+	private float movementSpeed = 4;
+
+	void Update() {
+		
+		transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+
+	}
+}
