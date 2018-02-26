86700a2b337f4e4db1ed23e813cef8de8e062d82
amelie-beh <amelie-behrendt@web.de>
2018-02-26 19:25:33 +0100

    EndTrigger vom Tutorial eingefügt

---
+++ Endless_Runner/Assets/Scripts/TutorialEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEnd : MonoBehaviour {
+
+	//If the player enters the trigger, the endscreen will be shown
+
+	public Canvas EndScreenCanvas;
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			EndScreenCanvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+		}
+	}
+}
