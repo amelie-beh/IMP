d8a419aa396d6967e669bca521df6a9399c28424
Chuchu_chan-PC\Chuchu_chan <isabell-jennifer94@web.de>
2018-02-27 19:50:57 +0100

    Highscore button hinzugefügt
    und ein skript gelöscht

--- Endless_Runner/Assets/Scripts/Main_Menu_Functions.cs
+++ Endless_Runner/Assets/Scripts/Main_Menu_Functions.cs
@@ -8,14 +8,17 @@ public class Main_Menu_Functions : MonoBehaviour {
 	//Script for the Menu functions of the MainMenu
 	public Slider audioSlider;
 	public float audio;
-	
+	public Text score;
 	public void Start(){
 		audio = PlayerPrefs.GetFloat("Audio");
 		audioSlider.value = audio;
+		score.text = (PlayerPrefs.GetInt("HS1").ToString() );
 	}
+	
 	public void StartGame(){
 		SceneManager.LoadScene(1);
 	}
+	
 	public void Quit(){
 		#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
@@ -23,11 +26,14 @@ public class Main_Menu_Functions : MonoBehaviour {
 			Application.Quit ();
 		#endif
 	}
+	
 	public void SliderIsUsed(){
 		PlayerPrefs.SetFloat("Audio", audioSlider.value);
 		PlayerPrefs.Save ();
 	}
+	
 	public void StartTutorial(){
 		SceneManager.LoadScene(2);
 	}
+	
 }
