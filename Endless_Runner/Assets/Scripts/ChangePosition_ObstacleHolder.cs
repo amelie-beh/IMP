ac1f623a4efac191c22fd4518900b4db97257d3d
amelie-beh <amelie-behrendt@web.de>
2018-02-28 19:03:34 +0100

    ObstacleHolder komplett randomisiert

--- Endless_Runner/Assets/Scripts/ChangePosition_ObstacleHolder.cs
+++ Endless_Runner/Assets/Scripts/ChangePosition_ObstacleHolder.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangePosition_ObstacleHolder : MonoBehaviour {
-
-	// If ObstacleHolder is behind the player's sight, the position will be changed to create a loop of ObstacleHolders
-
+	
 	public GameObject[] obstacleHolders;
 	public GameObject ObstacleHolderController;
 	public ChangePosition_Controller changePostitionController;
+	private int random1;
+	private int random2;
+	private int random3;
 
+	//Setting three random ObstacleHolders as active to start the game
+	void Start() {
+		changePostitionController.SetRandomActive ();
+		random1 = changePostitionController.r;
+		obstacleHolders[random1].transform.position = new Vector3 (0.5f, 3, 18);
+		changePostitionController.SetRandomActive ();
+		random2 = changePostitionController.r;
+		obstacleHolders[random2].transform.position = new Vector3 (0.5f, 3, 68);
+		changePostitionController.SetRandomActive ();
+		random3 = changePostitionController.r;
+		obstacleHolders[random3].transform.position = new Vector3 (0.5f, 3, 118);
+	}
+
+	// If ObstacleHolder is behind the player's sight, the position will be changed to create a loop of ObstacleHolders
 	void Update () {
 		for (int i = 0; i <= 9; i++) {
 			if (obstacleHolders[i].transform.position.z <= -32) {
